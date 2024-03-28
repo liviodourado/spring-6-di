99159a3139ed5a8b2de34646396f3f2f895712de
Livio Dourado <liviodourado@gmail.com>
2024-03-28 16:03:13 -0300

    feat: default profile

--- src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18nControllerTestEN.java
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/Myi18nControllerTestEN.java
@@ -3,9 +3,8 @@ package guru.springframework.spring6di.controllers.i18n;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("EN")
+// @ActiveProfiles("EN")
 @SpringBootTest
 public class Myi18nControllerTestEN {
 

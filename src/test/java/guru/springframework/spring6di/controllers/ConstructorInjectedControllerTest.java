c1cbaf691f2df11e3c388f9b26dd8aa8f1897eed
Livio Dourado <liviodourado@gmail.com>
2024-03-28 15:24:01 -0300

    feat: Injeção de dependência sem usar o Spring

---
+++ src/test/java/guru/springframework/spring6di/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void testSayHello() {
+        System.out.println(controller.sayHello());
+    }
+}
